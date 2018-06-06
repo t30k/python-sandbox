1ab63e3b87c21b113d9ebbe41d526bafa771914c
t30k <takawh7@gmail.com>
2018-06-06 17:20:07 +0900

    add yaml_importer

---
+++ yaml_importer.py
@@ -0,0 +1,14 @@
+import yaml
+
+def import_yml():
+    with open('test.yaml') as file:
+        data = yaml.load(file)
+        myeip = data['MyEIP']
+        type = myeip['Type']
+        print(type)
+        properties = myeip['Properties']
+        instanceid = properties['InstanceId']
+        print(instanceid)
+
+if __name__ == "__main__":
+    import_yml()
